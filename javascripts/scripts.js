65baa01dad2c2c423a61a3d9e91029dae0c7b9a7
Samson Quaye <squaye85@yahoo.com>
2011-09-06 19:36:43 +0100

    Added jQuery code for displaying part of the form

--- javascripts/scripts.js
+++ javascripts/scripts.js
@@ -1,3 +1,18 @@
 /**
  * @author SQuaye
  */
+$("document").ready(function(){
+	//Hides the new and exisiting datasource form 
+	$("div#datasourceInfo").css("display","none");
+	$("div#newDatasource").css("display","none");
+	//Hides or show a form based on the input from the user.
+	$("input[name=datasourceExist]").change(function(){
+		if($("input[name=datasourceExist]:checked").val() == "Yes"){
+			$("div#datasourceInfo").show();
+			$("div#newDatasource").css("display","none");
+		}else{
+			$("div#datasourceInfo").css("display","none");
+			$("div#newDatasource").show();
+		}
+	})
+})
